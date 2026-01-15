81d2d36c1570674fd8aaf0671a8604f4d8326d38
Lucid Themes <lucidtemplates@gmail.com>
2026-01-15 15:52:03 +0000

    feat(components): add login form component

---
+++ src/components/login-form.tsx
@@ -0,0 +1,29 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+
+export function LoginForm({ className, ...props }: React.ComponentProps<'form'>) {
+  return (
+    <form className={cn('flex flex-col gap-6', className)} {...props}>
+      <FieldGroup>
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input id="email" type="email" placeholder="you@example.com" required />
+        </Field>
+        <Field>
+          <div className="flex items-center">
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <a href="#" className="ml-auto text-sm underline-offset-4 hover:underline">
+              Forgot your password?
+            </a>
+          </div>
+          <Input id="password" type="password" required />
+        </Field>
+        <Field>
+          <Button type="submit">Login</Button>
+        </Field>
+      </FieldGroup>
+    </form>
+  );
+}
