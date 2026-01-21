c05cd0f18d76b4679df854d0aebc662cd274332c
Lucid Themes <lucidtemplates@gmail.com>
2026-01-21 16:21:36 +0000

    refactor(components): change main dashboard header to sticky

--- src/components/dashboard/main-header.tsx
+++ src/components/dashboard/main-header.tsx
@@ -21,7 +21,7 @@ const data = {
 
 export default function MainHeader() {
   return (
-    <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b-1 px-5 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    <header className="sticky top-0 z-1 flex h-16 shrink-0 items-center justify-between gap-2 border-b-1 bg-background px-5 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
