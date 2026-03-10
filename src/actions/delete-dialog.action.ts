4004e38eb91654e592c4ca3cab0a69cfa0b703a2
Lucid Themes <lucidtemplates@gmail.com>
2026-03-10 17:23:55 +0000

    refactor(actions): delete dialog action to delete from supabase

---
+++ src/actions/delete-dialog.action.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+import { createClient } from '@/lib/supabase/server';
+import { createLogEvent } from '@/lib/supabase/log-event';
+
+export async function deleteDialogDeleteRowFromTable(
+  rowId: string,
+  table: string,
+  path: string,
+  logEventName: string,
+  logEventMessage: string,
+  userId: string,
+) {
+  if (!rowId || !table) return { success: false };
+
+  const supabase = await createClient();
+
+  const { error } = await supabase.from(table).delete().eq('id', rowId);
+
+  if (error) {
+    createLogEvent('error', logEventName + '_FAILED', error.message, userId);
+
+    return { success: false };
+  }
+
+  revalidatePath(path);
+
+  createLogEvent('info', logEventName + '_SUCCESSFUL', logEventMessage + ' deleted. Id: ' + rowId, userId);
+
+  return { success: true };
+}
