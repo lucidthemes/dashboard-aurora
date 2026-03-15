5d7f8c215eff7fd681f2c07bd799f09e24fffb94
Lucid Themes <lucidtemplates@gmail.com>
2026-03-15 19:27:59 +0000

    feat(dashboard): customers route hooks

---
+++ src/app/(dashboard)/customers/hooks/view-sheet/use-logs.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+
+import getCustomerViewSheetLogs from '../../data/view-sheet/get-logs';
+import type { CustomerViewSheetLogs } from '../../schemas/view-sheet/logs.schema';
+
+export default function useCustomerViewSheetTabLogs(customerId: string) {
+  const viewSheetTabLogsQuery = useQuery<CustomerViewSheetLogs[]>({
+    queryKey: ['customersViewSheetTabLogs', customerId],
+    queryFn: () => getCustomerViewSheetLogs(customerId ?? null),
+    enabled: !!customerId,
+  });
+
+  return viewSheetTabLogsQuery;
+}
---
+++ src/app/(dashboard)/customers/hooks/view-sheet/use-reviews.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+
+import getCustomerViewSheetReviews from '../../data/view-sheet/get-reviews';
+import type { CustomerViewSheetReviews } from '../../schemas/view-sheet/reviews.schema';
+
+export default function useCustomerViewSheetTabReviews(customerId: string) {
+  const viewSheetTabReviewsQuery = useQuery<CustomerViewSheetReviews[]>({
+    queryKey: ['customersViewSheetTabReviews', customerId],
+    queryFn: () => getCustomerViewSheetReviews(customerId ?? null),
+    enabled: !!customerId,
+  });
+
+  return viewSheetTabReviewsQuery;
+}
