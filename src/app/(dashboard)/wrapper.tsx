2fbfdf4c667c709b1290b232d891ac1cbde03319
Lucid Themes <lucidtemplates@gmail.com>
2026-04-04 13:03:22 +0100

    refactor(dashboard): add dashboard error boundary

--- src/app/(dashboard)/wrapper.tsx
+++ src/app/(dashboard)/wrapper.tsx
@@ -11,7 +11,7 @@ export default function DashboardWrapper({ children }: { children: React.ReactNo
       <DashboardSidebar />
       <SidebarInset>
         <DashboardHeader />
-        <div className="p-5 sm:p-7.5 md:p-10">{children}</div>
+        <div className="h-full p-5 sm:p-7.5 md:p-10">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
