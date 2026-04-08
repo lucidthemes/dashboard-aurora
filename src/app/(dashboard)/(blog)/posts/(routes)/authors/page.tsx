ba0ddca5567249522f7b099496f11e07ab8dc041
Lucid Themes <lucidtemplates@gmail.com>
2026-04-08 12:13:25 +0100

    feat(dashboard): posts authors page route

---
+++ src/app/(dashboard)/(blog)/posts/(routes)/authors/page.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Authors',
+  description: 'View posts authors list',
+};
+
+export default function PostsAuthorsPage() {}
