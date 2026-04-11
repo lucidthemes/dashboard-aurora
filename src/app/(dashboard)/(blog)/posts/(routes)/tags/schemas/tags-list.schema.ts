f7e221528006ddf9e73f09e53922e02d39f42ac3
Lucid Themes <lucidtemplates@gmail.com>
2026-04-11 15:39:47 +0100

    feat(dashboard): posts tags route schemas

---
+++ src/app/(dashboard)/(blog)/posts/(routes)/tags/schemas/tags-list.schema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const PostsTagsListSchema = z.object({
+  id: z.uuid(),
+  name: z.string(),
+  slug: z.string(),
+  description: z.string(),
+  created_at: z.coerce.date(),
+});
+
+export type PostsTagsList = z.infer<typeof PostsTagsListSchema>;
