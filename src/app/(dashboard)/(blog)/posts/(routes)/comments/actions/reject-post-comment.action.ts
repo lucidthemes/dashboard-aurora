0473187527fab35225799cc1f6e40ac5de88d8fb
Lucid Themes <lucidtemplates@gmail.com>
2026-04-11 18:00:57 +0100

    feat(dashboard): posts comments route actions

---
+++ src/app/(dashboard)/(blog)/posts/(routes)/comments/actions/reject-post-comment.action.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import { getUserWithRole } from '@/lib/supabase/auth';
+import { createClient } from '@/lib/supabase/server';
+import { createLogEvent } from '@/lib/supabase/log-event';
+
+export default async function rejectPostComment(commentId: string) {
+  const { user, role } = await getUserWithRole();
+
+  if (!user || !role || !['admin', 'editor'].includes(role)) {
+    await createLogEvent('error', 'REJECT_POST_COMMENT_UNAUTHORIZED', 'Unauthorized user', user?.id);
+
+    return { success: false };
+  }
+
+  const supabase = await createClient();
+
+  const { error } = await supabase.from('post_comments').update({ status: 'rejected' }).eq('id', commentId);
+
+  if (error) {
+    await createLogEvent('error', 'REJECT_POST_COMMENT_FAILED', error.message + '. Comment id: ' + commentId, user.id);
+
+    return { success: false };
+  }
+
+  await createLogEvent('info', 'REJECT_POST_COMMENT_SUCCESSFUL', 'Comment rejected. Id: ' + commentId, user.id);
+
+  return { success: true };
+}
