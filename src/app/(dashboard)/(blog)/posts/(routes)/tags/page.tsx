93f0725cd30bae045d26194c423c8af4367ce323
Lucid Themes <lucidtemplates@gmail.com>
2026-04-11 15:38:13 +0100

    refactor(dashboard): add posts tags page route list

--- src/app/(dashboard)/(blog)/posts/(routes)/tags/page.tsx
+++ src/app/(dashboard)/(blog)/posts/(routes)/tags/page.tsx
@@ -1,8 +1,43 @@
 import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+import MainContainer from '@/app/(dashboard)/components/container';
+import ListSort from '@/components/list/sort';
+import { LoadingSpinner } from '@/components/loading';
+
+import PostsTagsPageWrapper from './components/page/wrapper';
+import PostsTagsPageHeading from './components/page/heading';
+import PostsTagsList from './components/list';
 
 export const metadata: Metadata = {
   title: 'Tags',
   description: 'View posts tags list',
 };
 
-export default function PostsTagsPage() {}
+export default async function PostsTagsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    page?: number;
+    limit?: number;
+    sort?: string;
+  }>;
+}) {
+  const { page = 1, limit = 12, sort = '' } = await searchParams;
+
+  return (
+    <MainContainer>
+      <PostsTagsPageWrapper>
+        <PostsTagsPageHeading />
+        <div className="flex flex-col gap-5">
+          <div className="flex self-end">
+            <ListSort />
+          </div>
+          <Suspense fallback={<LoadingSpinner />}>
+            <PostsTagsList page={page} limit={limit} sort={sort} />
+          </Suspense>
+        </div>
+      </PostsTagsPageWrapper>
+    </MainContainer>
+  );
+}
