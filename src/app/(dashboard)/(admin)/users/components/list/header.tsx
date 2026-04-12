2fe68103203d44b8624be9307b07b66c05643b88
Lucid Themes <lucidtemplates@gmail.com>
2026-04-12 18:21:28 +0100

    fix(dashboard): list header search error alignment

--- src/app/(dashboard)/(admin)/users/components/list/header.tsx
+++ src/app/(dashboard)/(admin)/users/components/list/header.tsx
@@ -7,9 +7,9 @@ import UsersListFilters from './filters';
 export default function UsersListHeader({ search }: { search?: string }) {
   return (
     <div className="flex flex-col justify-between gap-5 lg:flex-row">
-      <div className="flex flex-col items-center gap-5 lg:flex-row">
+      <div className="flex flex-col items-center items-start gap-5 lg:flex-row">
         <ListSearch placeholder="User ID or Email address" search={search} />
-        <Separator orientation="vertical" className="hidden h-6! lg:block" />
+        <Separator orientation="vertical" className="mt-2 hidden h-6! lg:block" />
         <UsersListFilters />
       </div>
       <ListSort />
