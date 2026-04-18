8c6d10ebb12a30a3ec92ee724ceab5280a988413
Lucid Themes <lucidtemplates@gmail.com>
2026-04-18 18:40:49 +0100

    refactor(dashboard): remove padding from post and page routes

--- src/app/(dashboard)/wrapper.tsx
+++ src/app/(dashboard)/wrapper.tsx
@@ -1,17 +1,23 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
+
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 import DashboardSidebar from './components/sidebar';
 import DashboardHeader from './components/header';
 
 export default function DashboardWrapper({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const contentPadding = !['/post', '/page'].includes(pathname) ? 'p-5 sm:p-7.5 md:p-10' : '';
+
   return (
     <SidebarProvider>
       <DashboardSidebar />
       <SidebarInset>
         <DashboardHeader />
-        <div className="h-full p-5 sm:p-7.5 md:p-10">{children}</div>
+        <div className={`h-full ${contentPadding}`}>{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
