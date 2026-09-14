c77544d217160f9a08f60cd11e8690cfb43e875e
Lucid Themes <lucidtemplates@gmail.com>
2026-09-14 17:13:17 +0100

    fix(dashboard): posts routes query range

--- src/app/(dashboard)/(blog)/posts/(routes)/categories/data/get-posts-categories.ts
+++ src/app/(dashboard)/(blog)/posts/(routes)/categories/data/get-posts-categories.ts
@@ -16,8 +16,8 @@ export default async function getPostsCategories(
 ): Promise<{ categories: PostsCategoriesList[]; totalCount: number }> {
   const supabase = await createClient();
 
-  const rangeFrom = (page - 1) * limit;
-  const rangeTo = rangeFrom + limit - 1;
+  const rangeFrom = (Number(page) - 1) * Number(limit);
+  const rangeTo = Number(rangeFrom) + Number(limit) - 1;
 
   const sortAsc = sort === 'date_asc' ? true : false;
 
